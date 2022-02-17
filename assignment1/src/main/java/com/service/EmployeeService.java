dcdcd0238d709e73e17ef524eada5d96462cedd2
stephenZhu2021 <zhuxciwei@gmail.com>
2022-02-17 09:42:20 -0500

    Update EmployeeService.java

--- assignment1/src/main/java/com/service/EmployeeService.java
+++ assignment1/src/main/java/com/service/EmployeeService.java
@@ -11,6 +11,7 @@ public class EmployeeService {
     public static List<Employee> employeeList = new ArrayList<>();
 
     public List<Employee> getAllEmployees(){
+        employeeList.clear();
         Employee emp1 = new Employee(1,"Ran","Zhu",
                 new Address(6550,"Sherbrooke West","H4B 1N6",new City("Montreal","QC")));
         Employee emp2 = new Employee(2,"Charlie","Postman",
